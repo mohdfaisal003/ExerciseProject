8cd33af630c9f5009c033e83e6f2972385e352e9
Mohd Faisal <fpecial3@gmail.com>
2024-01-08 08:29:15 +0530

    Initial

--- app/src/main/java/com/exerciseproject/mvvm/AuthViewModel.kt
+++ app/src/main/java/com/exerciseproject/mvvm/AuthViewModel.kt
@@ -2,55 +2,96 @@ package com.test.project.mvvm
 
 import android.app.Activity
 import android.util.Log
-import androidx.lifecycle.LiveData
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.firebase.FirebaseException
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthOptions
 import com.google.firebase.auth.PhoneAuthProvider
-import kotlinx.coroutines.Dispatchers
+import com.google.firebase.auth.PhoneAuthProvider.OnVerificationStateChangedCallbacks
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
-import kotlinx.coroutines.withContext
 import java.util.concurrent.TimeUnit
 
 class AuthViewModel : ViewModel() {
-    var authResult = MutableLiveData<AuthModel>()
+    private var verificationId = MutableLiveData<String?>()
+//    var authResult = MutableLiveData<AuthModel>()
+    var authResult = MutableLiveData<Boolean>()
     private val auth = FirebaseAuth.getInstance()
 
-    fun sendVerificationCode(activity: Activity, phoneNumber: String) {
+    fun sendVerificationCode(activity: Activity, phoneNumber: String, callBack: OnVerificationStateChangedCallbacks) {
+        Log.d("sendVerificationCodePhoneNumber","+91${phoneNumber}")
         viewModelScope.launch {
             try {
                 val options = PhoneAuthOptions.newBuilder(auth)
                     .setPhoneNumber("+91${phoneNumber}")
                     .setTimeout(60L, TimeUnit.SECONDS)
                     .setActivity(activity)
-                    .setCallbacks(object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-                        override fun onVerificationCompleted(phoneAuthCredential: PhoneAuthCredential) {
-                            auth.signInWithCredential(phoneAuthCredential).let {
-                                if (it.isSuccessful) {
-                                    Log.i("PhoneAuth", phoneAuthCredential.smsCode!!)
-                                    authResult.value = AuthModel(true,phoneAuthCredential.smsCode,"Success")
-                                } else {
-                                    authResult.value = AuthModel(false,null,"auth failed")
-                                }
-                            }
-                        }
-
-                        override fun onVerificationFailed(exception: FirebaseException) {
-                            authResult.value = AuthModel(false,null,"auth failed")
-                            Log.d("onVerificationFailed",exception.message.toString())
-                        }
-                    })
+                    .setCallbacks(callBack)
                     .build()
                 PhoneAuthProvider.verifyPhoneNumber(options)
+                Toast.makeText(activity,"OTP Sent to ${phoneNumber}",Toast.LENGTH_SHORT).show()
             } catch (exception: Exception) {
                 exception.printStackTrace()
-                authResult.value = AuthModel(false,null,"auth failed")
+                Log.e("PhoneAuthException",exception.message.toString())
             }
         }
     }
+
+    fun verifyCode(activity: Activity,verificationId: String,otp: String) {
+        Log.d("verifyCode",otp)
+        val credential = PhoneAuthProvider.getCredential(verificationId, otp)
+
+        signInWithCredential(activity,credential)
+    }
+
+    private fun signInWithCredential(activity: Activity,credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(OnCompleteListener<AuthResult?> { task ->
+                if (task.isSuccessful) {
+                    authResult.value = true
+                } else {
+                    authResult.value = false
+                    Toast.makeText(activity, task.exception!!.message, Toast.LENGTH_LONG)
+                        .show()
+                }
+            })
+    }
+
+//    fun sendVerificationCode(activity: Activity, phoneNumber: String) {
+//        viewModelScope.launch {
+//            try {
+//                val options = PhoneAuthOptions.newBuilder(auth)
+//                    .setPhoneNumber("+91${phoneNumber}")
+//                    .setTimeout(60L, TimeUnit.SECONDS)
+//                    .setActivity(activity)
+//                    .setCallbacks(object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+//                        override fun onVerificationCompleted(phoneAuthCredential: PhoneAuthCredential) {
+//                            Log.i("PhoneAuth", phoneAuthCredential.smsCode!!)
+////                            auth.signInWithCredential(phoneAuthCredential).let {
+////                                if (it.isSuccessful) {
+////                                    Log.i("PhoneAuth", phoneAuthCredential.smsCode!!)
+////                                    authResult.value = AuthModel(true,phoneAuthCredential.smsCode,"Success")
+////                                } else {
+////                                    authResult.value = AuthModel(false,null,"auth failed")
+////                                }
+////                            }
+//                        }
+//
+//                        override fun onVerificationFailed(exception: FirebaseException) {
+//                            authResult.value = AuthModel(false,null,"auth failed")
+//                            Log.d("onVerificationFailed",exception.message.toString())
+//                        }
+//                    })
+//                    .build()
+//                PhoneAuthProvider.verifyPhoneNumber(options)
+//            } catch (exception: Exception) {
+//                exception.printStackTrace()
+//                authResult.value = AuthModel(false,null,"auth failed")
+//            }
+//        }
+//    }
 }
---
+++ app/src/main/java/com/exerciseproject/ui/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.exerciseproject.ui
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.exerciseproject.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider.ForceResendingToken
+import com.google.firebase.auth.PhoneAuthProvider.OnVerificationStateChangedCallbacks
+import com.test.project.mvvm.AuthViewModel
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    val authViewModel by viewModels<AuthViewModel>()
+    var verificationId: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        authViewModel.authResult.observe(this, Observer {
+            if (it) {
+                Toast.makeText(this, "Successfully Authenticated", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Auth Failed", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        binding.idBtnGetOtp .setOnClickListener {
+            if (binding.idEdtPhoneNumber.text.isNotEmpty()) {
+//                authViewModel.sendVerificationCode(this, binding.idEdtPhoneNumber.text.toString())
+                authViewModel.sendVerificationCode(this, binding.idEdtPhoneNumber.text.toString(),phoneAuthCallBack)
+            } else {
+                Toast.makeText(this, "Please enter a valid mobile number", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+        binding.idBtnVerify.setOnClickListener {
+            if (binding.idEdtOtp.text.isNotEmpty()) {
+                authViewModel.verifyCode(this, verificationId.toString(),binding.idEdtOtp.text.toString())
+            }
+        }
+    }
+
+
+    private val phoneAuthCallBack: OnVerificationStateChangedCallbacks = object : OnVerificationStateChangedCallbacks() {
+
+            override fun onCodeSent(id: String, forceResendingToken: ForceResendingToken) {
+                super.onCodeSent(id, forceResendingToken)
+                Log.d("onCodeSent",id)
+                verificationId = id
+            }
+            override fun onVerificationCompleted(phoneAuthCredential: PhoneAuthCredential) {
+                Log.d("onVerificationCompleted",phoneAuthCredential.smsCode.toString())
+                val otp = phoneAuthCredential.smsCode
+                if (otp != null) {
+                    binding.idEdtOtp.setText(otp)
+                }
+            }
+            override fun onVerificationFailed(exception: FirebaseException) {
+                Log.e("onVerificationFailed",exception.message.toString())
+                Toast.makeText(this@MainActivity, exception.message, Toast.LENGTH_LONG).show()
+            }
+        }
+}
